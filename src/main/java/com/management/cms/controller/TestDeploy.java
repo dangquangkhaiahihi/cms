a43c5b49afcde6446d3fd029345c6d1654772063
dangquangkhaiahihi <42770800+dangquangkhaiahihi@users.noreply.github.com>
2022-10-24 23:48:10 +0700

    test deploy

---
+++ src/main/java/com/management/cms/controller/TestDeploy.java
@@ -0,0 +1,35 @@
+package com.management.cms.controller;
+
+import com.management.cms.security.UserDetailsImpl;
+import com.management.cms.utils.Utils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestDeploy {
+    //    @PreAuthorize("@permissionServiceImpl.businessPremisesView")
+    @GetMapping("/view")
+    public String viewTest(){
+        return "VIEW";
+    }
+
+    //    @PreAuthorize("@permissionServiceImpl.businessPremisesCreate")
+    @GetMapping("/create")
+    public String createTest(){
+        return "CREATE";
+    }
+
+    //    @PreAuthorize("@permissionServiceImpl.businessPremisesEdit")
+    @GetMapping("/edit")
+    public String editTest(){
+        return "EDIT";
+    }
+
+    @GetMapping("/test")
+    public UserDetailsImpl test(){
+        return Utils.getUserDetail();
+    }
+
+}
